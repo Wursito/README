c6aae3e562730d1c2fb48d0a865e1d04f186fb9d
Wursito <77864987+Wursito@users.noreply.github.com>
2021-11-30 18:05:24 +0100

    Creacion log.log en main activity con la val peli, para que me devuelva la variable introducida en este caso el titulo

--- app/src/main/java/es/spain/club/MainActivity.kt
+++ app/src/main/java/es/spain/club/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import dagger.hilt.android.AndroidEntryPoint
+import es.spain.domain.usecase.GetFilmUseCase
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -11,10 +12,17 @@ class MainActivity : AppCompatActivity() {
 
     @Inject
     lateinit var log: MyLog
+    @Inject
+    lateinit var usecase: GetFilmUseCase
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         log.log("Oncreate")
+        val film = usecase.execute()
+        log.log("El titulo es ${film.title}")
+        
 
     }
 
---
+++ data/src/main/java/es/spain/data/DataModule.kt
@@ -0,0 +1,17 @@
+package es.spain.data
+
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityComponent
+import es.spain.domain.repository.FilmRepository
+
+@Module
+@InstallIn(ActivityComponent::class)
+
+
+
+abstract class DataModule {
+    @Binds
+    abstract fun bindFilmRepository(impl: FilmRepositoryImpl): FilmRepository
+}
