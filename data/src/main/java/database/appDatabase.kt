7bcbaf796813e515951a84a755769b7ff452cc49
Wursito <77864987+Wursito@users.noreply.github.com>
2022-02-08 17:42:40 +0100

    12.3

---
+++ data/src/main/java/database/appDatabase.kt
@@ -0,0 +1,11 @@
+package database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [FilmEntity::class], version = 1)
+abstract class AppDatabase: RoomDatabase(){
+
+    abstract fun filmDao(): FilmDao
+
+}
