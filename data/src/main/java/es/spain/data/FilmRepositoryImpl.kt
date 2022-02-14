12fa82cd0349f96725dd271596c8557d02f79c35
Wursito <77864987+Wursito@users.noreply.github.com>
2022-02-14 19:45:11 +0100

    12.5

--- data/src/main/java/es/spain/data/FilmRepositoryImpl.kt
+++ data/src/main/java/es/spain/data/FilmRepositoryImpl.kt
@@ -2,13 +2,16 @@ package es.spain.data
 
 import android.icu.text.CaseMap
 import android.media.Rating
+import database.DatabaseDataSource
 import es.spain.domain.entity.Film
 import es.spain.domain.repository.FilmRepository
 import javax.inject.Inject
 
 class FilmRepositoryImpl @Inject constructor(
 
-private val serverDataSource: ServerDataSource): FilmRepository {
+
+    private val databaseDataSource: DatabaseDataSource,
+    private val serverDataSource: ServerDataSource): FilmRepository {
     override suspend fun getfilm(id: Int, language: String): Film? {
         return runCatching {
         serverDataSource.getFilm(id, language)
@@ -16,12 +19,22 @@ private val serverDataSource: ServerDataSource): FilmRepository {
 
     }
 
-    override suspend fun getFilms(language: String): List<Film>? {
+    override suspend fun getFilm(id: Int, language: String):Film? {
         return runCatching {
-            serverDataSource.getFilms(language)
-        }.getOrNull()
+            val filmfromServer = serverDataSource.getFilm(id,language)
+            databaseDataSource.updateFilms(listOf(filmfromServer))
+            filmfromServer
+        }.getOrNull() ?: databaseDataSource.getFilm(id)
     }
 
+
+    override suspend fun getFilms(language: String): List<Film>? {
+        return runCatching {
+            val filmsfromServer = serverDataSource.getFilms(language)
+            databaseDataSource.updateFilms(filmsfromServer)
+            filmsfromServer
+        }.getOrNull() ?: databaseDataSource.getFilms()
+    }
 
 
 }
